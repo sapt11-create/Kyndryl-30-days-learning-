0f45c81a0ad68696737a4336cdddc2ab89625b18
sapt11-create <saptarshi.ghosh@kyndryl.com>
2026-09-25 15:34:08 +0530

    Day 3 Basic python script

---
+++ day3basicpythonscript.py
@@ -0,0 +1,10 @@
+name = input("Enter your name: ")
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+result = num1 + num2
+
+print("Name:", name)
+print("First number:", num1)
+print("Second number:", num2)
+print("Result:", result)
